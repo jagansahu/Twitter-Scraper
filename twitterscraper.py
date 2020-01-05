d86eb7666ec071d1294fdece357857fed8b39ce7
jagansahu <37058620+jagansahu@users.noreply.github.com>
2020-01-05 01:56:25 -0500

    Created Twitter Scraper
    Scrapes the 10 most recent tweets.

---
+++ twitterscraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import urllib
+import urllib.request
+
+theurl = 'https://twitter.com/itsawesomepixs'
+source = urllib.request.urlopen(theurl)
+soup = BeautifulSoup(source, 'html.parser')
+
+name = soup.title.text
+
+i = 1
+print("10 MOST RECENT TWEETS OF\n" + name + "\n")
+for tweets in soup.find_all('div', class_='content'):
+  print(i)
+  print(tweets.find('p').text)
+  print()
+  i += 1
+  if i == 11:
+    break
+  
+  
