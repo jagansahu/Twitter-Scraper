aeb7e9d9795609c346e0a9b7bb4aeacf66de2489
jagansahu <37058620+jagansahu@users.noreply.github.com>
2020-01-05 02:00:18 -0500

    updated

--- twitterscraper.py
+++ twitterscraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 import urllib.request
 
-theurl = 'https://twitter.com/itsawesomepixs'
+theurl = 'https://twitter.com/UWaterlooNews'
 source = urllib.request.urlopen(theurl)
 soup = BeautifulSoup(source, 'html.parser')
 
