7b0419354b8287fa446acea661cafe04f107deb3
jagansahu <37058620+jagansahu@users.noreply.github.com>
2020-01-17 10:25:19 -0500

    Updated twitterscraper.py

--- twitterscraper.py
+++ twitterscraper.py
@@ -2,20 +2,21 @@ from bs4 import BeautifulSoup
 import urllib
 import urllib.request
 
-theurl = 'https://twitter.com/UWaterlooNews'
+theurl = 'https://twitter.com/UWaterloo'
 source = urllib.request.urlopen(theurl)
 soup = BeautifulSoup(source, 'html.parser')
 
 name = soup.title.text
-
+numb = int(input("How many tweets do you want to print?\n"))
 i = 1
-print("10 MOST RECENT TWEETS OF\n" + name + "\n")
+substring = "These Tweets are protected"
+print("10 MOST RECENT TWEETS AND RETWEETS OF\n" + name + "\n")
+
 for tweets in soup.find_all('div', class_='content'):
   print(i)
   print(tweets.find('p').text)
   print()
   i += 1
-  if i == 11:
-    break 
-  
+  if i == numb+1:
+    break
   
