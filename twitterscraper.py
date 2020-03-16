1aa55cdad6d2f432e07e7f37ef79ca6232281930
jagansahu <37058620+jagansahu@users.noreply.github.com>
2020-03-16 02:05:36 -0400

    Updated twitter scraper

--- twitterscraper.py
+++ twitterscraper.py
@@ -3,15 +3,34 @@ import urllib
 import urllib.request
 import csv
 
-theurl = 'https://twitter.com/UWaterloo'
-source = urllib.request.urlopen(theurl)
+#asks for twitter user 
+while True: 
+  try:
+    t_user = input("Enter the username of the Twitter User you wish to view tweets of.\n (MUST BE A VALID TWITTER USER)\n");
+    theurl = 'https://twitter.com/' + t_user
+    source = urllib.request.urlopen(theurl)
+  except:
+    print("\nPlease enter a valid Twitter username.\n______________________________________\n")
+    continue
+  else:
+    break
+
 soup = BeautifulSoup(source, 'html.parser')
 
 name = soup.title.text 
-numb = int(input("How many tweets do you want to print?\n")) #takes in number of tweets to be printed
+
+#takes in number of tweets you want it to output
+while True:
+  try:
+    numb = int(input("How many tweets do you want to print?\n")) #takes in number of tweets to be printed
+  except:
+    print("\nPlease enter a valid number only.\n_________________________________\n")
+  else:
+    break
+
 i = 1
 substring = "These Tweets are protected"
-print("10 MOST RECENT TWEETS AND RETWEETS OF\n" + name + "\n")
+print(str(numb) + " MOST RECENT TWEETS AND RETWEETS OF\n" + name + "\n")
 
 file = open("list_of_tweets.csv", "w") #opens csv file
 writer = csv.writer(file)
